3ca5a5ab40c21b4798612f812d7e170d5796cb54
Tomasz Kłak <tomasz@tomaszklak.pl>
2021-12-03 16:37:02 +0100

    Day 3 speedup

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,11 +1,17 @@
+use smallvec::SmallVec;
 use std::io::{stdin, BufRead};
 use std::time::Instant;
 
 fn main() {
-    let nums: Vec<Vec<char>> = stdin()
+    let nums: Vec<SmallVec<[u8; 12]>> = stdin()
         .lock()
         .lines()
-        .map(|s| s.unwrap().chars().collect())
+        .map(|s| {
+            s.unwrap()
+                .chars()
+                .map(|c| c.to_digit(2).unwrap() as u8)
+                .collect()
+        })
         .collect();
 
     let s = Instant::now();
@@ -19,28 +25,18 @@ fn main() {
     println!("{:?}", ea + eb);
 }
 
-fn most_frequent_bit(n: usize, nums: &[Vec<char>]) -> char {
+fn most_frequent_bit(n: usize, nums: &[SmallVec<[u8; 12]>]) -> u8 {
     let mut f = [0usize; 2];
-    for num in nums {
-        if num[n] == '0' {
-            f[0] += 1;
-        } else {
-            f[1] += 1;
-        }
-    }
-    if f[0] > f[1] {
-        '0'
-    } else {
-        '1'
-    }
+    nums.iter().for_each(|num| f[num[n] as usize] += 1);
+    (f[0] > f[1]) as u8
 }
 
-fn part_one(nums: &[Vec<char>]) -> i64 {
+fn part_one(nums: &[SmallVec<[u8; 12]>]) -> i64 {
     let len = nums[0].len();
     let mut gamma = String::new();
     let mut epsilon = String::new();
     for i in 0..len {
-        if most_frequent_bit(i, nums) == '0' {
+        if most_frequent_bit(i, nums) == 0 {
             gamma.push('0');
             epsilon.push('1');
         } else {
@@ -53,21 +49,27 @@ fn part_one(nums: &[Vec<char>]) -> i64 {
     gamma * epsilon
 }
 
-fn part_two(all: &[Vec<char>]) -> i64 {
+fn part_two(all: &[SmallVec<[u8; 12]>]) -> i64 {
     let mut most = all.to_vec();
     let mut least = all.to_vec();
     for i in 0..all[0].len() {
         if most.len() > 1 {
             let b = most_frequent_bit(i, &most);
-            most = most.iter().filter(|n| n[i] == b).cloned().collect();
+            most = most.into_iter().filter(|n| n[i] == b).collect();
         }
         if least.len() > 1 {
             let b = most_frequent_bit(i, &least);
-            least = least.iter().filter(|n| n[i] != b).cloned().collect();
+            least = least.into_iter().filter(|n| n[i] != b).collect();
         }
     }
-    let most: String = most[0].iter().cloned().collect();
-    let least: String = least[0].iter().cloned().collect();
+    let most: String = most[0]
+        .iter()
+        .map(|b| if *b == 0 { '0' } else { '1' })
+        .collect();
+    let least: String = least[0]
+        .iter()
+        .map(|b| if *b == 0 { '0' } else { '1' })
+        .collect();
     let most = i64::from_str_radix(&most, 2).unwrap();
     let least = i64::from_str_radix(&least, 2).unwrap();
     most * least
