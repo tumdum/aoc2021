b395450256931fb9079959b1cb32b9175a9880e0
Tomasz Kłak <tomasz@tomaszklak.pl>
2021-12-03 08:45:45 +0100

    day 3

---
+++ day03/src/main.rs
@@ -0,0 +1,66 @@
+use std::io::{stdin, BufRead};
+
+fn main() {
+    let nums: Vec<Vec<char>> = stdin()
+        .lock()
+        .lines()
+        .map(|s| s.unwrap().chars().collect())
+        .collect();
+
+    println!("{}", part_one(&nums));
+    println!("{}", part_two(&nums));
+}
+
+fn most_frequent_bit(n: usize, nums: &[Vec<char>]) -> char {
+    let mut f = [0usize; 2];
+    for num in nums {
+        if num[n] == '0' {
+            f[0] += 1;
+        } else {
+            f[1] += 1;
+        }
+    }
+    if f[0] > f[1] {
+        '0'
+    } else {
+        '1'
+    }
+}
+
+fn part_one(nums: &[Vec<char>]) -> i64 {
+    let len = nums[0].len();
+    let mut gamma = String::new();
+    let mut epsilon = String::new();
+    for i in 0..len {
+        if most_frequent_bit(i, nums) == '0' {
+            gamma.push('0');
+            epsilon.push('1');
+        } else {
+            gamma.push('1');
+            epsilon.push('0');
+        }
+    }
+    let gamma = i64::from_str_radix(&gamma, 2).unwrap();
+    let epsilon = i64::from_str_radix(&epsilon, 2).unwrap();
+    gamma * epsilon
+}
+
+fn part_two(all: &[Vec<char>]) -> i64 {
+    let mut most = all.to_vec();
+    let mut least = all.to_vec();
+    for i in 0..all[0].len() {
+        if most.len() > 1 {
+            let b = most_frequent_bit(i, &most);
+            most = most.iter().filter(|n| n[i] == b).cloned().collect();
+        }
+        if least.len() > 1 {
+            let b = most_frequent_bit(i, &least);
+            least = least.iter().filter(|n| n[i] != b).cloned().collect();
+        }
+    }
+    let most: String = most[0].iter().cloned().collect();
+    let least: String = least[0].iter().cloned().collect();
+    let most = i64::from_str_radix(&most, 2).unwrap();
+    let least = i64::from_str_radix(&least, 2).unwrap();
+    most * least
+}
