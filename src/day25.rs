10b5510e047e5a8889532f529dd6fbac79849328
Tomasz Kłak <tomasz@tomaszklak.pl>
2021-12-25 07:20:34 +0100

    day25 speedup

--- src/day25.rs
+++ src/day25.rs
@@ -1,7 +1,9 @@
 use std::io::BufRead;
 use std::time::{Duration, Instant};
 
-fn canonaical(m: &[Vec<char>], (mut x, mut y): (i32,i32)) -> (i32,i32) {
+type M = [[u8;139]; 137];
+
+fn canonaical(m: &M, (mut x, mut y): (i32,i32)) -> (i32,i32) {
     let h = m.len() as i32;
     let w = m[0].len() as i32;
     if x < 0 {
@@ -17,13 +19,13 @@ fn canonaical(m: &[Vec<char>], (mut x, mut y): (i32,i32)) -> (i32,i32) {
     (x,y)
 }
 
-fn get(m: &[Vec<char>], (x, y): (i32,i32)) -> char {
+fn get(m: &M, (x, y): (i32,i32)) -> u8 {
     let (x,y) = canonaical(m, (x,y));
     m[y as usize][x as usize]
 }
 
 #[allow(dead_code)]
-fn print(m: &[Vec<char>]) {
+fn print(m: &M) {
     let h = m.len() as i32;
     let w = m[0].len() as i32;
 
@@ -36,34 +38,36 @@ fn print(m: &[Vec<char>]) {
 
 }
 
-fn next_pos(m: &[Vec<char>], (x,y): (i32,i32)) -> Option<(i32,i32)> {
+fn next_pos(m: &M, (x,y): (i32,i32)) -> Option<(i32,i32)> {
     let c = get(m, (x,y));
     match c {
-        '>' => Some(canonaical(m, (x+1, y))),
-        'v' => Some(canonaical(m, (x, y+1))),
+        b'>' => Some(canonaical(m, (x+1, y))),
+        b'v' => Some(canonaical(m, (x, y+1))),
         _ => None,
     }
 }
 
-fn step(m: &[Vec<char>]) -> Vec<Vec<char>> {
+fn step(m: &M) -> M {
     let h = m.len() as i32;
+    debug_assert_eq!(137,h);
     let w = m[0].len() as i32;
-    let mut ret = vec![vec!['.'; w as usize]; h as usize];
+    debug_assert_eq!(139,w);
+    let mut ret = [[b'.';139]; 137];
 
     for y in 0..h {
         for x in 0..w {
             let c = get(&m,(x,y));
-            if c == 'v' {
-                ret[y as usize][x as usize] = 'v';
+            if c == b'v' {
+                ret[y as usize][x as usize] = b'v';
             }
-            if c != '>' {
+            if c != b'>' {
                 continue;
             }
             let next = next_pos(&m, (x,y)).unwrap();
             let next_c = get(&m, next);
-            if next_c == '.' {
+            if next_c == b'.' {
                 ret[next.1 as usize][next.0 as usize] = c;
-                ret[y as usize][x as usize] = '.';
+                ret[y as usize][x as usize] = b'.';
             } else {
                 ret[y as usize][x as usize] = c;
             }
@@ -71,19 +75,19 @@ fn step(m: &[Vec<char>]) -> Vec<Vec<char>> {
     }
     
     let m = &ret;
-    let mut ret = m.to_vec();
+    let mut ret = m.clone();
 
     for y in 0..h {
         for x in 0..w {
             let c = get(&m,(x,y));
-            if c != 'v' {
+            if c != b'v' {
                 continue;
             }
             let next = next_pos(&m, (x,y)).unwrap();
             let next_c = get(&m, next);
-            if next_c == '.' {
+            if next_c == b'.' {
                 ret[next.1 as usize][next.0 as usize] = c;
-                ret[y as usize][x as usize] = '.';
+                ret[y as usize][x as usize] = b'.';
             } else {
                 ret[y as usize][x as usize] = c;
             }
@@ -94,19 +98,24 @@ fn step(m: &[Vec<char>]) -> Vec<Vec<char>> {
 }
 
 pub fn solve(input: &mut dyn BufRead, verify_expected: bool, output: bool) -> Duration {
-    let mut input: Vec<Vec<char>> = input.lines().map(|s| s.unwrap().chars().collect()).collect();
-
+    let input: Vec<Vec<char>> = input.lines().map(|s| s.unwrap().chars().collect()).collect();
+    let mut map: M = [[b'.'; 139]; 137];
+    for y in 0..137 {
+        for x in 0..139 {
+            map[y][x] = input[y][x] as u8;
+        }
+    }
 
     let s = Instant::now();
 
     let mut part1 = 0;
     for s in 1.. {
-        let next = step(&input);
-        if next == input {
+        let next = step(&map);
+        if next == map {
             break;
         }
         part1 = s + 1;
-        input = next;
+        map = next;
     }
 
 
